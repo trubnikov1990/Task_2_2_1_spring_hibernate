522b6bd4081e3f5c2d8dff8dd1f69f51dbd96d47
Dmitriy Trubnikov <truq1990@gmail.com>
2023-12-02 23:30:00 +0400

    The END

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -10,33 +10,33 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
 
-  public static void main(String[] args) throws SQLException {
-    AnnotationConfigApplicationContext context =
-        new AnnotationConfigApplicationContext(AppConfig.class);
+      UserService userService = context.getBean(UserService.class);
 
-    UserService userService = context.getBean(UserService.class);
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("VW", 1)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("BMW", 22)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru" , new Car("OPEL", 333)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru" , new Car("MERCEDES", 4444)));
 
-    userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("VW", 1)));
-    userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("BMW", 22)));
-    userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("OPEL", 333)));
-    userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("MERCEDES", 4444)));
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println("Car  " + user.getUserCar());
+         System.out.println();
+      }
 
-    List<User> users = userService.listUsers();
-    for (User user : users) {
-      System.out.println("Id = " + user.getId());
-      System.out.println("First Name = " + user.getFirstName());
-      System.out.println("Last Name = " + user.getLastName());
-      System.out.println("Email = " + user.getEmail());
-      System.out.println("Car  " + user.getUserCar());
-      System.out.println();
-    }
+      for (User user : userService.listUserCar("VW", 1)) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+      }
 
-    for (User user : userService.listUserCar("VW", 1)) {
-      System.out.println("Id = " + user.getId());
-      System.out.println("First Name = " + user.getFirstName());
-    }
 
-    context.close();
-  }
+      context.close();
+   }
 }
